8b9c7389584c866ef5618ea1061e424aa0eb3455
Bryan J. Brown <bjbrown@fsu.edu>
2024-09-12 16:28:18 -0500

    Pretty Printin

--- coverdump.py
+++ coverdump.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import pdfuncoverer
+import pprint
 import sys
 
 file = sys.argv[1]
 coverpage = pdfuncoverer.dump_coverpage_metadata(file)
-print(coverpage)
+pprint.pprint(coverpage)
--- pdfuncoverer.py
+++ pdfuncoverer.py
@@ -1,13 +1,13 @@
-from pypdf import PdfReader
+import pypdf
 
 def dump_coverpage_metadata(file):
-  reader = PdfReader(file)
+  reader = pypdf.PdfReader(file)
   page1 = reader.pages[0]
   coverpage_metadata = page1
   return coverpage_metadata
 
 def has_coverpage(file):
-  reader = PdfReader(file)
+  reader = pypdf.PdfReader(file)
   coverpage_emails = ['mailto:lib-ir@fsu.edu', 'mailto:lib-support@fsu.edu']
   coverpage = False
   page1 = reader.pages[0]
