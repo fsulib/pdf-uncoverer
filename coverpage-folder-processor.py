cfbca303b8f4c7ae9a27b2ba173452bb977b38bd
Bryan J. Brown <bjbrown@fsu.edu>
2024-09-13 13:19:21 -0500

    Finishes folder processor

--- coverpage-folder-processor.py
+++ coverpage-folder-processor.py
@@ -4,21 +4,35 @@ import funcs
 import sys
 import os
 import shutil
+import subprocess
 
 folder = sys.argv[1]
 if not os.path.exists(folder):
-  sys.exit("{} does not exist.".format(folder))
+  sys.exit('{} does not exist.'.format(folder))
 if not os.path.isdir(folder):
-  sys.exit("{} is not a folder.".format(folder))
+  sys.exit('{} is not a folder.'.format(folder))
 
-procfolder = "{}_processed".format(folder)
+procfolder = '{}_processed'.format(folder)
 if os.path.exists(procfolder):
   shutil.rmtree(procfolder)
 os.makedirs(procfolder) 
 
+counts = {'total': 0, 'uncovered': 0, 'decovered': 0}
+
 for file in os.listdir(folder):
-  if file.endswith(".pdf"):
-    if funcs.has_fsul_coverpage("{}/{}".format(folder, file)):
-      print("{} DOES have FSUL coverpage".format(file))
+  counts['total'] += 1
+  print('')
+  if file.endswith('.pdf'):
+    if not funcs.has_fsul_coverpage('{}/{}'.format(folder, file)):
+      counts['uncovered'] += 1
+      print('{} does not have an FSUL coverpage.'.format(file))
     else:
-      print("{} DOES NOT have FSUL coverpage".format(file))
+      counts['decovered'] += 1
+      print('{} has an FSUL coverpage.'.format(file))
+      subprocess.run(['pdftk', '{}/{}'.format(folder, file), 'cat', '2-end', 'output', '{}/{}'.format(procfolder, file)]) 
+      print('{} has has been copied to {} without coverpage.'.format(file, procfolder))
+
+print('')
+print('Total PDFs in {}: {}'.format(folder, counts['total']))
+print('Total PDFs skipped due to lack of FSUL coverpage: {}'.format(counts['uncovered']))
+print('Total PDFs processed into {} to remove FSUL coverpage: {}'.format(procfolder, counts['decovered']))
